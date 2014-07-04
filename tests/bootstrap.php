441e97232bdbd927a7ccbcd1d197712d5e9cffb1
rob <rob@rob-PC>
2014-07-04 16:53:00 +0100

    Doc block, line endings and whitespace clean up

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,6 @@
 <?php
 
-set_include_path(implode(PATH_SEPARATOR, array( 
+set_include_path(implode(PATH_SEPARATOR, array(
     get_include_path(),
     realpath(dirname(__FILE__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'src'),
 )));
