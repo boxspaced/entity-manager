f336b114d5074e43f9d063a54422b97e45233ccb
rw <rw@rw-PC.home>
2014-09-25 20:53:42 +0100

    Require zf1 for testing

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,9 +1,13 @@
 <?php
 
+$basePath = realpath(dirname(__FILE__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR);
+
 set_include_path(implode(PATH_SEPARATOR, array(
     get_include_path(),
-    realpath(dirname(__FILE__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'src'),
+    $basePath . DIRECTORY_SEPARATOR . 'src',
 )));
+
+require $basePath . DIRECTORY_SEPARATOR . 'vendor/autoload.php';
 
 spl_autoload_register(function ($class) {
     $classPath = str_replace('_', DIRECTORY_SEPARATOR, $class);
